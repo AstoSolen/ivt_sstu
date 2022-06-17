dc9c9c0c8dd50727e0a46a70c7ec2381c747d49d
dmitriev <a.u.dmitriev@voskhod.ru>
2022-06-17 18:10:48 +0400

    task 8, 17, 18, 20 completed

--- src/com/company/Task17.java
+++ src/com/company/Task17.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import com.company.util.MatrixUtil;
+
 public class Task17 {
 
     public static void main(String[] args) {
-        // TODO: 15.06.2022 not yet implemented
+        int[][] matrix = MatrixUtil.createAndFillMatrix(7);
+        MatrixUtil.printMatrix(matrix);
+        System.out.println("Average is " + findAvg(matrix));
+    }
+
+    protected static double findAvg(int[][] matrix) {
+        double sum = 0;
+        double count = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            for (int j = 0; j < row.length / 2 + 1; j++) {
+                int elem = row[j];
+                if (j < i && j < matrix.length - 1 - i) {
+                    sum += elem;
+                    count++;
+                }
+            }
+        }
+
+        return sum / count;
     }
 }
--- src/com/company/Task20.java
+++ src/com/company/Task20.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import com.company.util.MatrixUtil;
+
 public class Task20 {
 
     public static void main(String[] args) {
-        // TODO: 15.06.2022 not yet implemented
+        int[][] matrix = MatrixUtil.createAndFillMatrix(7);
+        MatrixUtil.printMatrix(matrix);
+        System.out.println("Average is " + findAvg(matrix));
+    }
+
+    protected static double findAvg(int[][] matrix) {
+        double sum = 0;
+        double count = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            for (int j = row.length / 2; j < row.length; j++) {
+                int elem = row[j];
+                if (j >= i && j >= matrix.length - 1 - i) {
+                    System.out.printf("elem[%d] = %d%n", j, elem);
+                    sum += elem;
+                    count++;
+                }
+            }
+        }
+
+        return sum / count;
     }
 }
