6a46c8085e344c147c36f2e9997c8747ee228bae
dmitriev <a.u.dmitriev@voskhod.ru>
2022-06-17 14:30:42 +0400

    task 1, 3, 10,21 completed

--- src/com/company/Task1.java
+++ src/com/company/Task1.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import com.company.util.MatrixUtil;
+
 public class Task1 {
 
     public static void main(String[] args) {
-        // TODO: 15.06.2022 not yet implemented
+        int[][] matrix = MatrixUtil.createAndFillMatrix(7);
+        MatrixUtil.printMatrix(matrix);
+        System.out.println("Average is " + findAvg(matrix));
+    }
+
+    protected static double findAvg(int[][] matrix) {
+        double sum = 0;
+        double count = 0;
+
+        for (int i = matrix.length / 2; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            for (int j = 0; j < row.length; j++) {
+                int elem = row[j];
+                if (j >= (row.length - i - 1) && j <= i) {
+                    sum += elem;
+                    count++;
+                }
+            }
+        }
+
+        return sum / count;
     }
 }
--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import com.company.util.MatrixUtil;
+
 public class Task3 {
 
     public static void main(String[] args) {
-        // TODO: 15.06.2022 not yet implemented
+        int[][] matrix = MatrixUtil.createAndFillMatrix(7);
+        MatrixUtil.printMatrix(matrix);
+        System.out.println("Average is " + findAvg(matrix));
+    }
+
+    protected static double findAvg(int[][] matrix) {
+        double sum = 0;
+        double count = 0;
+
+        for (int i = 0; i < matrix.length / 2 + 1; i++) {
+            int[] row = matrix[i];
+            for (int j = 0; j < row.length; j++) {
+                int elem = row[j];
+                if (j <= (row.length - i - 1) && j >= i) {
+                    sum += elem;
+                    count++;
+                }
+            }
+        }
+
+        return sum / count;
     }
 }
